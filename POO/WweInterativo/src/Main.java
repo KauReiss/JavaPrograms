2993a75817412703830b0ca8c0a98e1f066988b9
Kauan Reis <kauan.reiss@hotmail.com>
2025-01-13 16:54:04 -0300

    first version classes Raw and Smackdown

--- POO/WweInterativo/src/Main.java
+++ POO/WweInterativo/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Raw s1 = new Raw("Dolph Zigler", "Canadá", 32, 1.79f, 78.3f);
+        Smackdown s2 = new Smackdown("Undertaker", "EUA", 48, 1.93f, 98.3f);
+
+        s1.apresentar();
+        s2.apresentar();
     }
 }
--- POO/WweInterativo/src/Raw.java
+++ POO/WweInterativo/src/Raw.java
@@ -3,19 +3,42 @@ public class Raw extends Superstar {
     private String divisao;
 
 
-    public Raw(String nome, String nacionalidade, int idade, float altura, float peso, String divisao) {
+    public Raw(String nome, String nacionalidade, int idade, float altura, float peso) {
         super(nome, nacionalidade, idade, altura, peso);
         this.divisao = "RAW";
     }
 
+    public String getDivisao() {
+        return divisao;
+    }
+    public void setDivisao(String divisao) {
+        this.divisao = divisao;
+    }
+    
     @Override
     public void apresentar() {
-        
+        System.out.println(
+        "\n Superstar: " + getNome() + 
+        "\n Nacionalidade: " + getNacionalidade() +
+        "\n Idade: " + getIdade() + " anos " + 
+        "\n Altura: " + getAltura() + "m" +
+        "\n Peso: " + getPeso() + "kg" + 
+        "\n Divisão: " + getDivisao() +
+        "\n Vitorias: " + getVitorias() +
+        "\n Derrotas: " + getDerrotas()
+        );
     }
 
     @Override
     public void status() {
-        
+        System.out.println( 
+        "\n " + getNome() + " superstar do " + getDivisao() + 
+        "\n " + getVitorias() + " triunfos" +
+        "\n " + getDerrotas() + " derrotas"
+        );
     }
-    
 }
+
+    
+
+    
