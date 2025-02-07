7d2626dddea1ac31e4f1edd06f811d973bc28f12
Kauan Reis <kauan.reiss@hotmail.com>
2025-02-06 23:33:47 -0300

    implements class Read

--- PROJETOS/FileJava/src/Arquivo.java
+++ PROJETOS/FileJava/src/Arquivo.java
@@ -1,3 +1,34 @@
-public class Arquivo {
-    
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Arquivo {   
+    public static String Read(String Caminho) {
+        String conteudo = "";
+        try {
+            FileReader arq = new FileReader(Caminho);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha = "";
+            try {
+                linha = lerArq.readLine();
+                while (linha!=null) {
+                    conteudo += linha;
+                    linha = lerArq.readLine();
+                }
+                arq.close();
+            } catch (IOException ex) {
+                conteudo = "Erro: Não foi possível ler o arquivo!";
+            } 
+        } catch (FileNotFoundException ex) {
+            conteudo = "Erro: Arquivo não encontrado!";
+        }
+        if (conteudo.contains("Erro")) {
+            System.out.println(conteudo);
+            return "";
+        }
+        else {
+            return conteudo;
+        }
+    }
 }
