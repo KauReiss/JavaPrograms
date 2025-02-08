a255fe20bc943bd5c6c60b3dcd3a9345dd58ba56
Kauan Reis <kauan.reiss@hotmail.com>
2025-02-07 23:13:33 -0300

    test write txt main

--- PROJETOS/FileJava/src/Main.java
+++ PROJETOS/FileJava/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String arq = "test.txt";
+
+        String texto = "Lorem ipsum dolor sit amet. Id eveniet alias qui omnis amet eum internos labore in voluptates perferendis. Et esse debitis At itaque omnis vel ipsa itaque et quas laudantium.\r\n" +
+                        "\r\n" +
+                        "Est obcaecati assumenda nam incidunt aperiam qui modi cumque id asperiores nobis. Id nulla quia aut temporibus omnis est veniam minus. Et animi commodi est repellat dolorum quo iste quae ea reprehenderit harum et natus impedit eum excepturi suscipit.\r\n";
+
+        if (Arquivo.Write(arq, texto)) {
+            System.out.println("Arquivo gravado com sucesso!");
+        } else {
+            System.out.println("Erro ao salvar o arquivo");
+        }
     }
 }
